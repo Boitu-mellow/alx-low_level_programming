565dfc84f7548236c79e8047d86e40471c0f0dbc
Boitu-mellow <boitumelomashaba23@gmail.com>
2023-09-24 09:55:22 -0700

    9-print_comb.c commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Description: Print all combinations of one-digit numbers, no type char
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(11);
+			putchar(24);
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
