13f6be69a079022ca03f6be3fffb472ac2ae20f5
Boitu-mellow <boitumelomashaba23@gmail.com>
2023-09-24 08:27:42 -0700

    1-last_digit.c commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,6 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
 
-	printf("Last digit of %d is %d ", n, n lastigit);
 	if (lastdigit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 	else if (lastdigit == 0)
