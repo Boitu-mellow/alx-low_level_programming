992c516bc8094a786b9ca233786af0000eddac46
Boitu-mellow <boitumelomashaba23@gmail.com>
2023-09-24 17:38:15 -0700

    1-alphabet.c commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet
+* function that prints the alphabet, in lowercase, followed by a new line
+* Return: void
+*/
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
