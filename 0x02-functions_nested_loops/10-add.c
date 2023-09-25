a366545b9f4e1d73e660afed04672ab60fb185db
Boitu-mellow <boitumelomashaba23@gmail.com>
2023-09-24 19:40:43 -0700

    10-add.c commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - sum of two intergers
+ * @n: integer 1
+ * @m: integer 2
+ * Return: sum of n and m
+ */
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+
+	return (sum);
+}
