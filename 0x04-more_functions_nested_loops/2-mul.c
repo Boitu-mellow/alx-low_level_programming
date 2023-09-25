e8ae3c11941d50f9e63cd6793e8d8a0cd4408127
Boitu-mellow <boitumelomashaba23@gmail.com>
2023-09-25 16:48:30 -0700

    2-mul.c commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ *mul - function that multiplies two integers.
+ *@x:integer to multiply the second
+ *@y:integer to multiply the first
+ *Return: the product of multiplicatio
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
