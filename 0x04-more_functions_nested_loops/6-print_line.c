37efffe796395a95c127fa9c70730ad46596bdbf
Boitu-mellow <boitumelomashaba23@gmail.com>
2023-09-25 17:39:30 -0700

    6-print_line.c commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws straight line in terminal
+ * @n: number of times the character should be printed
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int n;
+
+	for (n = 1; n <= n; n++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
+
+}
